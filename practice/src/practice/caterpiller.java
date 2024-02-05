7682184af016b0cb9b48d86b87867d46cb086485
Dave Goes <goesdave.12@@gmail.com>
2024-02-05 10:37:38 +0530

    comnmit

---
+++ practice/src/practice/caterpiller.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class caterpiller {
+
+	int moves,cabbage;
+	
+          
+	caterpiller(int moves,int cabbage){
+		this.moves=moves;
+		this.cabbage=cabbage;
+		
+	}
+
+}
